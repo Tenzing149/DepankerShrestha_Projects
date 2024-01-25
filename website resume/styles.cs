892cf6364c796c74ff62fd4fbf14fbe3127cc58b
Depanker S <depankershrestha@gmail.com>
2024-01-25 11:37:13 -0500

    Testing Commit

---
+++ website resume/styles.cs
@@ -0,0 +1,42 @@
+/* google fonts */
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap');
+
+/* Variables css */
+
+:root {
+    --header-height: 3rem;
+
+    /* colors */
+    /* change favorite color */
+    --hue-color: 250; /*Purple 250 - Green 142 - Blue 230 - Pink 340*/
+
+    /* HSL color mode */
+    --first-color: hsl(var(--hue-color), 69%, 61%);
+    --first-color-second: hsl(var(--hue-color), 69%, 61%);
+    --first-color-alt: hsl(var(--hue-color), 57%, 53%);
+    --first-color-lighter: hsl(var(--hue-color), 92%, 85%);;
+    --title-color: hsl(var(--hue-color), 8%, 15%);;
+    --text-color-light: hsl(var(--hue-color), 8%, 65%);
+    --input-color: hsl(var(--hue-color), 70%, 96%);
+    --body-color: hsl(var(--hue-color), 60%, 99%);
+    --container-color: #FFF;
+
+    /* Font and typography */
+    --body-font: 'Poppins', sans-serif;
+
+    /* .5rem = 8px, 1rem =16px, 1.5rem = 24px ...*/
+    --big-font-size: 2rem;
+    --h1-font-size: 1.5rem;
+    --h2-font-size: 1.25rem;
+    --h3-font-size: ;
+    --normal-font-size: ;
+    --small-font-size: ;
+    --smaller-font-size: ;
+
+    /* Font weight */
+    --font-medium: ;
+    --font-semi-bold: ;
+
+
+
+}
